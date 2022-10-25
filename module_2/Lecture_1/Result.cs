c6a3b5b9aebd70d9cc8d99df7c0969a88618e1c9
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2022-10-25 23:44:52 +0300

    Implement Result class

--- module_2/Lecture_1/Result.cs
+++ module_2/Lecture_1/Result.cs
@@ -2,4 +2,31 @@
 
 public class Result
 {
+    private readonly DateTime _time;
+    private readonly string _origin;
+    private readonly string _message;
+    private readonly ResultType _type;
+    private readonly bool _status;
+
+    public Result(string origin, string message, ResultType type, bool status = true)
+    {
+        _time = DateTime.Now;
+        _origin = origin;
+        _message = message;
+        _type = type;
+        _status = status;
+    }
+
+    public enum ResultType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public override string ToString()
+    {
+        var errorString = _status ? string.Empty : "Action failed by a reason: ";
+        return $"{_time}: {_type}: \"{errorString}{_message}\": {_origin}";
+    }
 }
