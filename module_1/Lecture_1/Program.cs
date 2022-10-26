697813c456b3df38c79e0563abed8fc3e6ffbda8
Las04ka <75950858+Las04ka@users.noreply.github.com>
2022-10-26 13:13:53 +0000

    Delete module_1/Lecture_1 directory

--- module_1/Lecture_1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿var name = "Oleksandr";
-var surname = "Yovbak";
-var age = 18;
-var yearsUntil40 = 40 - age;
-Console.WriteLine($"My name is {name} {surname}, I am {age} years old.");
-Console.WriteLine($"I have {yearsUntil40} years until my new life.");
